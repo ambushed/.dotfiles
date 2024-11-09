09b2938eaef84c86731da74764a89282f931698a
Vlad <vlad@MacBook-Pro.local>
2024-11-09 10:48:39 +0100

    working on zsh config and installation script

---
+++ config/zsh/.zshenv
@@ -0,0 +1,42 @@
+export CLICOLOR=1 # enable color support for ls.
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export ZDOTDIR="$HOME/.config/zsh"
+export ZSH_CACHE_DIR="$HOME/.cache/zsh"
+
+export DOTFILES=${HOME}/.dotfiles
+
+if which rg >/dev/null; then
+  export RIPGREP_CONFIG_PATH=${DOTFILES}/.config/rg/.ripgreprc
+fi
+
+path+=(
+  /usr/local/bin
+)
+
+#-------------------------------------------------------------------------------
+#  FZF Settings
+#-------------------------------------------------------------------------------
+# --files: List files that would be searched but do not search
+# --no-ignore: Do not respect .gitignore, etc...
+# --hidden: Search hidden files and folders
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
+export FZF_DEFAULT_OPTS="--reverse \
+--cycle \
+--bind=esc:abort \
+--height 60% \
+--border sharp \
+--prompt '∷ ' \
+--marker ' ' \
+--pointer ▶ "
+
+# CTRL-/ to toggle small preview window to see the full command
+# CTRL-Y to copy the command into clipboard using pbcopy
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}' --preview-window up:3:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --color header:italic
+  --header 'Press CTRL-Y to copy command into clipboard'"
+
+export FZF_TMUX_OPTS='-p80%,60%'
---
+++ install.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+# Might as well ask for password up-front, right?
+echo "Starting install script, please grant me sudo access..."
+sudo -v
+
+exists() {
+  type "$1" &> /dev/null;
+}
+
+if [ "$(uname)" == "Darwin" ]; then
+  if exists brew; then
+    echo "Homebrew is installed!"
+  else
+    echo "Homebrew is not installed, installing now"
+    echo "This may take a while"
+    echo "Homebrew requires osx command lines tools, please download xcode first"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  fi
+fi
+
+packages=(
+  "curl"
+  "git"
+  "gcc"
+  "python"
+  #"lazygit"
+  #"thefuck"
+  #"delta"
+  "ripgrep" #binary is "rg" so will try to install every time :shrug:
+)
+
+
+install_missing_packages() {
+  for p in "${packages[@]}"; do
+    if exists $p; then
+      echo "$p is installed"
+    else
+      echo "$p is not installed"
+      # Detect the platform (similar to $OSTYPE)
+      case "$(uname)" in
+        'Linux')
+          apt install "$p" || echo "$p failed to install"
+          ;;
+        'Darwin')
+          brew install "$p" || echo "$p failed to install"
+          ;;
+        *) ;;
+      esac
+      echo "---------------------------------------------------------"
+      echo "Done "
+      echo "---------------------------------------------------------"
+    fi
+  done
+}
+
+install_missing_packages || echo "failed to install missing packages"
+
+export DOTFILES="$HOME/.dotfiles"
+
+if [ -d "$DOTFILES" ]; then
+  echo "Dotfiles have already been cloned into the home dir"
+else
+  echo "Cloning dotfiles"
+  git clone https://github.com/ambushed/dotfiles.git ~/.dotfiles
+fi
+
+cd "$DOTFILES" || "Didn't cd into dotfiles this will be bad :("
+git submodule update --init --recursive
+pwd
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+# Install fzf
+#
+if ! exists fzf; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~ /.fzf/install
+else 
+  echo "FZF is already installed"
+fi
+
+if [ "$(basename $SHELL)" == "zsh" ]; then
+  echo "Running zsh. No need to change shell!"
+else
+  echo "---------------------------------------------------------"
+  echo "Changing to zsh"
+  echo "---------------------------------------------------------"
+  chsh -s "$(which zsh)"
+fi
+
+$DOTFILES/install
+
+echo "---------------------------------------------------------"
+echo "All done!"
+echo "---------------------------------------------------------"
